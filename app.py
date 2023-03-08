3f5c17bdabff28e0ec835e25aa7fd9fda1939c67
YOUSEF ALSARRAJ <yousef.edu@outlook.com>
2023-03-08 15:38:15 +0300

    IDK

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 import os 
 from deeplearning import object_detection
-# webserver gateway interface
+
 app = Flask(__name__)
 
 BASE_PATH = os.getcwd()
 UPLOAD_PATH = os.path.join(BASE_PATH,'static/upload/')
 
+@app.route('/api/object_detection', methods=['POST'])
+def detect_text():
+    if 'image_name' not in request.files:
+        return jsonify({'error': 'No file uploaded'}), 400
+    
+    upload_file = request.files['image_name']
+    filename = upload_file.filename
+    path_save = os.path.join(UPLOAD_PATH, filename)
+    upload_file.save(path_save)
+    
+    text_list = object_detection(path_save, filename)
+    os.remove(path_save)
+    
+    return jsonify({'text_list': text_list})
 
-@app.route('/',methods=['POST','GET'])
-def index():
-    if request.method == 'POST':
-        upload_file = request.files['image_name']
-        filename = upload_file.filename
-        path_save = os.path.join(UPLOAD_PATH,filename)
-        upload_file.save(path_save)
-        text_list = object_detection(path_save,filename)
-        
-        print(text_list)
-
-        return render_template('index.html',upload=True,upload_image=filename,text=text_list,no=len(text_list))
-
-    return render_template('index.html',upload=False)
-
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
